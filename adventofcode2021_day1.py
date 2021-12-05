5363bd016e20a36eca37578d57ed19477f3f5838
Puuppo Joonas Antti Juhana <joonas.a.j.puuppo@student.jyu.fi>
2021-12-05 08:00:46 +0200

    Renamed functions

--- adventofcode2021_day1.py
+++ adventofcode2021_day1.py
@@ -1,19 +1,19 @@
-def countNumberOfIncreases(numbers):
+def count_number_of_increases(numbers):
     count = 0
     for i in range(1, len(numbers)):
         if (numbers[i] > numbers[i-1]):
             count = count + 1
     return count
 
-def countNumberOfSumIncreases(numbers):
+def count_number_of_sum_increases(numbers):
     count = 0
     sums = []
     for i in range(len(numbers) - 2):
         sum = numbers[i] + numbers[i+1] + numbers[i+2]
         sums.append(sum)
-    return countNumberOfIncreases(sums)
+    return count_number_of_increases(sums)
 
-def readNumbersFromFile(filename):
+def read_numbers_from_file(filename):
     numbers = []
     file = open(filename, "r")
     for line in file:
@@ -21,8 +21,8 @@ def readNumbersFromFile(filename):
     file.close()
     return numbers
 
-numbers = readNumbersFromFile("adventofcode2021_day1_input.txt")
-print(countNumberOfIncreases(numbers))
-print(countNumberOfSumIncreases(numbers))
+numbers = read_numbers_from_file("adventofcode2021_day1_input.txt")
+print(count_number_of_increases(numbers))
+print(count_number_of_sum_increases(numbers))
 
 
