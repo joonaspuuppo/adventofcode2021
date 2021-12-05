50d15c9fd7bd616ba1678eccb205768c7adeb247
Puuppo Joonas Antti Juhana <joonas.a.j.puuppo@student.jyu.fi>
2021-12-05 07:37:33 +0200

    Solved Day 1 Part 2

--- adventofcode2021_day1.py
+++ adventofcode2021_day1.py
@@ -5,6 +5,14 @@ def countNumberOfIncreases(numbers):
             count = count + 1
     return count
 
+def countNumberOfSumIncreases(numbers):
+    count = 0
+    sums = []
+    for i in range(len(numbers) - 2):
+        sum = numbers[i] + numbers[i+1] + numbers[i+2]
+        sums.append(sum)
+    return countNumberOfIncreases(sums)
+
 def readNumbersFromFile(filename):
     numbers = []
     file = open(filename, "r")
@@ -15,5 +23,6 @@ def readNumbersFromFile(filename):
 
 numbers = readNumbersFromFile("adventofcode2021_day1_input.txt")
 print(countNumberOfIncreases(numbers))
+print(countNumberOfSumIncreases(numbers))
 
 
